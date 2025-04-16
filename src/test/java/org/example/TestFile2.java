e97ac850c1d8d1e64a17285e665ced67ccd6b4fa
rakesh-mindfire <mfsirakeshpradhan@gmail.com>
2025-04-16 11:13:58 +0530

    Cleared all files

--- src/test/java/org/example/TestFile2.java
+++ src/test/java/org/example/TestFile2.java
@@ -1,10 +1,6 @@
 package org.example;
 
 public class TestFile2 {
-    //added a line
-    //added second line
-    //Some code
-//Added some line
-    //Added last line
-    //Added last line
+
+
 }
