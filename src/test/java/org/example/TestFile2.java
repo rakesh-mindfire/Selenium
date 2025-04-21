6badcd377ec8462a9ae2f585a80718946c32a769
rakesh-mindfire <mfsirakeshpradhan@gmail.com>
2025-04-21 11:03:50 +0530

    clear

--- src/test/java/org/example/TestFile2.java
+++ src/test/java/org/example/TestFile2.java
@@ -1,13 +1,5 @@
 package org.example;
 
 public class TestFile2 {
-    //First line
-    //Second line
-    //Third line
-    //First line
-    //Second line
-    //Third line
-    //Added a line
-    //added 2 line
 
 }
